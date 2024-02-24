82082d3751aa2a1ae4e2bb3fe97b360ebb6887fc
Josh McKinney <joshka@users.noreply.github.com>
2024-02-23 19:51:54 -0800

    add host redirect

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use axum::{
-    http::{header, HeaderMap, StatusCode},
-    response::{self, Html, IntoResponse},
+    extract::{Host, Request},
+    http::{header, HeaderMap, StatusCode, Uri},
+    middleware::{self, Next},
+    response::{self, Html, IntoResponse, Redirect, Response},
     routing::get,
     Router,
 };
@@ -8,6 +10,45 @@ use tower_http::trace::{
     DefaultMakeSpan, DefaultOnFailure, DefaultOnRequest, DefaultOnResponse, TraceLayer,
 };
 use tracing::{error, info, Level};
+
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    tracing_subscriber::fmt::init();
+    let include_headers = DefaultMakeSpan::new()
+        .level(Level::INFO)
+        .include_headers(true);
+    let trace_layer = TraceLayer::new_for_http()
+        .make_span_with(include_headers)
+        .on_request(DefaultOnRequest::new().level(Level::INFO))
+        .on_response(DefaultOnResponse::new().level(Level::INFO))
+        .on_failure(DefaultOnFailure::new().level(Level::ERROR));
+    let app = Router::new()
+        .route("/", get(readme))
+        .route("/feed", get(feed))
+        .route_layer(middleware::from_fn(redirect_host_name))
+        .layer(trace_layer);
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
+    info!("Listening on {}", listener.local_addr()?);
+    axum::serve(listener, app).await.unwrap();
+    Ok(())
+}
+
+// replace the old host name with the current one (and keep the rest of the request the same)
+async fn redirect_host_name(Host(host): Host, request: Request, next: Next) -> Response {
+    if host.to_lowercase() == "xkcd-with-alt-text.joshka.net" {
+        let uri = Uri::builder()
+            .scheme("https")
+            .authority("xkcd-with-alt-text.joshka.net")
+            .path_and_query(request.uri().path_and_query().unwrap().to_string())
+            .build()
+            .unwrap()
+            .to_string();
+        return Redirect::permanent(&uri).into_response();
+    }
+
+    next.run(request).await
+}
 
 async fn readme() -> Html<String> {
     let readme = include_str!("../README.md");
@@ -56,26 +97,4 @@ async fn feed(headers: HeaderMap) -> response::Result<impl IntoResponse> {
     let feed = feed.to_string();
 
     Ok(([(header::CONTENT_TYPE, content_type)], feed).into_response())
-}
-
-#[tokio::main]
-async fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
-    tracing_subscriber::fmt::init();
-    let include_headers = DefaultMakeSpan::new()
-        .level(Level::INFO)
-        .include_headers(true);
-    let trace_layer = TraceLayer::new_for_http()
-        .make_span_with(include_headers)
-        .on_request(DefaultOnRequest::new().level(Level::INFO))
-        .on_response(DefaultOnResponse::new().level(Level::INFO))
-        .on_failure(DefaultOnFailure::new().level(Level::ERROR));
-    let app = Router::new()
-        .route("/", get(readme))
-        .route("/feed", get(feed))
-        .layer(trace_layer);
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
-    info!("Listening on {}", listener.local_addr()?);
-    axum::serve(listener, app).await.unwrap();
-    Ok(())
 }
